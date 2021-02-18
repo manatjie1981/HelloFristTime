465f9dbd47d5958d91794a6cd1972b47e1a3b465
Manat Jieranaiwongkul-APNS (Thailand) <ManatJ-APNS@azay.co.th>
2021-02-18 09:53:13 +0700

    create connection 3

--- src/main/java/com/howtodoinjava/demo/controller/EmployeeController.java
+++ src/main/java/com/howtodoinjava/demo/controller/EmployeeController.java
@@ -27,17 +27,14 @@ public class EmployeeController {
 			e.printStackTrace();
 		}
 
-
-		str = connectDB();
-
 		employeesList.add(new Employee(1,str,"gupta","howtodoinjava@gmail.com"));
 		return employeesList;
     }
 
 	private static Connection getConnection() throws URISyntaxException, Exception {
-		Class.forName("com.mysql.jdbc.Driver");
-		String dbUrl =  "mysql://us-cdbr-east-03.cleardb.com/heroku_70451cd8b61281c";
-		return DriverManager.getConnection(dbUrl, "b93dc6f07df6ea", "313d4648");
+		Class.forName("org.postgresql.Driver");
+		String dbUrl =   System.getenv("mysql://b93dc6f07df6ea:313d4648@us-cdbr-east-03.cleardb.com/heroku_70451cd8b61281c?reconnect=true");
+		return DriverManager.getConnection(dbUrl);
 	}
 
 	private String connectDB() {
